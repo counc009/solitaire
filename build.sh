a69258ecbb063e00e502b4b5170fc8f35a48d132
Aaron Councilman <counc009@umn.edu>
2021-08-06 22:02:58 -0500

    Improve build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-javac -d ./build `find . -name '*.java'`
+javac -d ./build `find ./src -name '*.java'`
 cd build
 jar cfe solitaire.jar com.acc240.solitaire.Game `find . -name '*.class'`
